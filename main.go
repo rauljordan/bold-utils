7c52ed7f22184ca39460655b5878bb1243c05c19
Raul Jordan <raul@prysmaticlabs.com>
2023-12-13 10:13:58 -0600

    utils

--- main.go
+++ main.go
@@ -128,6 +128,7 @@ func mintStakeToken() {
 		if err != nil {
 			panic(err)
 		}
+		fmt.Printf("Nonce %d\n", txOpts.Nonce.Uint64())
 		fmt.Printf("Suggested gas price: %s gwei, bumping by %d percent\n", weiToGwei(suggested).String(), bumpPricePercent)
 		txOpts.GasPrice = bumpGasPrice(suggested)
 		fmt.Printf("Bumped to price: %s gwei\n", weiToGwei(txOpts.GasPrice).String())
@@ -163,7 +164,11 @@ func mintStakeToken() {
 		if err != nil {
 			panic(err)
 		}
+		fmt.Printf("Nonce %d\n", txOpts.Nonce.Uint64())
+		fmt.Printf("Suggested gas price: %s gwei, bumping by %d percent\n", weiToGwei(suggested).String(), bumpPricePercent)
+		txOpts.Nonce = new(big.Int).Add(txOpts.Nonce, big.NewInt(1))
 		txOpts.GasPrice = bumpGasPrice(suggested)
+		fmt.Printf("Bumped to price: %s gwei\n", weiToGwei(txOpts.GasPrice).String())
 		tx, err = tokenBindings.Approve(txOpts, rollupAddr, maxUint256)
 		if err != nil {
 			panic(err)
@@ -175,7 +180,11 @@ func mintStakeToken() {
 		if err != nil {
 			panic(err)
 		}
+		fmt.Printf("Nonce %d\n", txOpts.Nonce.Uint64())
+		fmt.Printf("Suggested gas price: %s gwei, bumping by %d percent\n", weiToGwei(suggested).String(), bumpPricePercent)
 		txOpts.GasPrice = bumpGasPrice(suggested)
+		fmt.Printf("Bumped to price: %s gwei\n", weiToGwei(txOpts.GasPrice).String())
+		txOpts.Nonce = new(big.Int).Add(txOpts.Nonce, big.NewInt(1))
 		tx, err = tokenBindings.Approve(txOpts, chalManagerAddr, maxUint256)
 		if err != nil {
 			panic(err)
